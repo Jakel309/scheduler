1e44e522257fc895e26e7432c7b9a1949c70d0e2
justinmc995 <justin.mc995@gmail.com>
2015-11-02 16:49:06 -0600

    order by

--- queries.sql
+++ queries.sql
@@ -44,7 +44,7 @@ and st.`Banner ID` = en.`Banner ID`
 and se.`Subject Code` = 'CS' and se.`Course Number` = 374 and se.`Section Number` = 01
 )
 group by se.`Begin Time 1`, se.`End Time1`, se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`, se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`
-order by st.`Last Name`, st.`First Name`, se.`Begin Time 1`;
+order by count(*) desc, se.`Begin Time 1`;
 
 /* This query should return the number of students that have a class at a certain day/time by classifcation */
 select count(*) as "# of Students", st.`Class Code`, se.`Begin Time 1`, se.`End Time1`,
@@ -62,4 +62,4 @@ and st.`Banner ID` = en.`Banner ID`
 and se.`Subject Code` = 'CS' and se.`Course Number` = 374 and se.`Section Number` = 01
 )
 group by st.`Class Code`, se.`Begin Time 1`, se.`End Time1`, se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`, se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`
-order by st.`Last Name`, st.`First Name`, se.`Begin Time 1`;
+order by count(*) desc, st.`Class Code`, se.`Begin Time 1`;
