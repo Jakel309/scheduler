6e06df2991369a574b7917f7c644e9b2c9bfb732
justinmc995 <justin.mc995@gmail.com>
2015-11-03 11:50:11 -0600

    Added a monster query that can find potential times for a class to move to

--- queries.sql
+++ queries.sql
@@ -78,3 +78,31 @@ where ((se.`Monday Ind1` = 'M' and se.`Wednesday Ind1` = 'W' and se.`Friday Ind1
 or (se.`Tuesday Ind1` = 'T' and se.`Thursday Ind1` = 'R' and se.`Monday Ind1` != 'M' and se.`Wednesday Ind1` != 'W' and se.`Friday Ind1` != 'F'))
 and se.`Begin Time 1` >= 800 and se.`Begin Time 1` <= 1600 and ((se.`End Time1` - se.`Begin Time 1`) = 50 or (se.`End Time1` - se.`Begin Time 1`) = 120)
 order by se.`Begin Time 1`;
+
+/* Find possible times that the class can be moved to by checking schedules of all students in a class and comparing with a list of ideal time slots */
+select distinct se.`Begin Time 1`, se.`End Time1`,
+se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`,
+se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`
+from section as se
+where ((se.`Monday Ind1` = 'M' and se.`Wednesday Ind1` = 'W' and se.`Friday Ind1` = 'F' and se.`Tuesday Ind1` != 'T' and se.`Thursday Ind1` != 'R')
+or (se.`Tuesday Ind1` = 'T' and se.`Thursday Ind1` = 'R' and se.`Monday Ind1` != 'M' and se.`Wednesday Ind1` != 'W' and se.`Friday Ind1` != 'F'))
+and se.`Begin Time 1` >= 800 and se.`Begin Time 1` <= 1600 and ((se.`End Time1` - se.`Begin Time 1`) = 50 or (se.`End Time1` - se.`Begin Time 1`) = 120)
+and (se.`Begin Time 1`, se.`End Time1`, se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`, se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`) not in 
+(
+	select se.`Begin Time 1`, se.`End Time1`,
+	se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`, se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`
+	from section as se, enrollment as en, student as st
+	where (se.`CRN` = en.`CRN` and se.`Term Code` = en.`Term Code`)
+	and st.`Banner ID` = en.`Banner ID`
+	and se.`Term Code` = 201610
+	and st.`Banner ID` in
+	(
+		select st.`Banner ID`
+		from section as se, student as st, enrollment as en
+		where (se.`CRN` = en.`CRN` and se.`Term Code` = en.`Term Code`)
+		and st.`Banner ID` = en.`Banner ID`
+		and se.`Subject Code` = 'CS' and se.`Course Number` = 374 and se.`Section Number` = 01
+	)
+	order by st.`Last Name`, st.`First Name`, se.`Begin Time 1`
+)
+order by se.`Begin Time 1`;
