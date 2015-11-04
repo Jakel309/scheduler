30137959a04c58a15c747bdc695753bdaf4621fa
Ethan Rutherford <ethanrutherford2@yahoo.com>
2015-11-04 12:18:00 -0600

    fix one last "merge conflict"

--- features/step_definitions/getStud_definition.rb
+++ features/step_definitions/getStud_definition.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Given(/^the input "([^"]*)"$/) do |arg1|
   @input=arg1
 end
@@ -10,17 +9,4 @@ end
 
 Then(/^the output should be "([^"]*)"$/) do |arg1|
   expect(@output).to eq(arg1)
-=======
-Given(/^the input "([^"]*)"$/) do |arg1|
-  @input=arg1
-end
-
-When(/^the program is run$/) do
-  @output = `/usr/bin/python getStud.py #{@input}`
-  raise('Command Failed!') unless $?.success?
-end
-
-Then(/^the output should be "([^"]*)"$/) do |arg1|
-  expect(@output).to eq(arg1)
->>>>>>> 7827a4af786e6a8ce8e303d5a54363774bf33afa
 end
