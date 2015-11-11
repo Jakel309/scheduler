0e765095b9c7dc2337c41d2fe39111fb79096038
justinmc995 <justin.mc995@gmail.com>
2015-11-11 10:14:50 -0600

    Time finder checks against everything, not just MWF and TR

--- queries.sql
+++ queries.sql
@@ -184,7 +184,7 @@ where
 and se.`Begin Time 1` >= 800 and se.`Begin Time 1` <= 1600
 and (se.`Begin Time 1`, se.`End Time1`, se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`, se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`) not in 
 (	/* Rule out times with student conflicts */
-	select t1.`Begin Time 1`, t1.`End Time1`,
+	select distinct t1.`Begin Time 1`, t1.`End Time1`,
 	t1.`Monday Ind1`, t1.`Tuesday Ind1`, t1.`Wednesday Ind1`, t1.`Thursday Ind1`, t1.`Friday Ind1`, t1.`Saturday Ind1`, t1.`Sunday Ind1`
 	from
 	(
@@ -198,9 +198,9 @@ and (se.`Begin Time 1`, se.`End Time1`, se.`Monday Ind1`, se.`Tuesday Ind1`, se.
 		)
 	) as t1,
 	(
-		select se.`Begin Time 1`, se.`End Time1`,
+		select st.`Class Code`, se.`Begin Time 1`, se.`End Time1`,
 		se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`, se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`
-    	from section as se, enrollment as en, student as st
+        from section as se, enrollment as en, student as st
     	where (se.`CRN` = en.`CRN` and se.`Term Code` = en.`Term Code`)
 		and st.`Banner ID` = en.`Banner ID`
 		and se.`Term Code` = 201610
@@ -212,11 +212,6 @@ and (se.`Begin Time 1`, se.`End Time1`, se.`Monday Ind1`, se.`Tuesday Ind1`, se.
 			and st.`Banner ID` = en.`Banner ID`
 			and se.`Subject Code` = 'CS' and se.`Course Number` = 374 and se.`Section Number` = 01
 			and se.`Term Code` = 201610
-		)
-		and
-		(
-			not (se.`Monday Ind1` = 'M' and se.`Wednesday Ind1` = 'W' and se.`Friday Ind1` = 'F' and se.`Tuesday Ind1` != 'T' and se.`Thursday Ind1` != 'R' and (se.`End Time1` - se.`Begin Time 1`) = 50)
-			and not (se.`Tuesday Ind1` = 'T' and se.`Thursday Ind1` = 'R' and se.`Monday Ind1` != 'M' and se.`Wednesday Ind1` != 'W' and se.`Friday Ind1` != 'F' and (se.`End Time1` - se.`Begin Time 1`) = 120)
 		)
 	) as t2
 	where
@@ -234,26 +229,10 @@ and (se.`Begin Time 1`, se.`End Time1`, se.`Monday Ind1`, se.`Tuesday Ind1`, se.
 		(t1.`Begin Time 1` between t2.`Begin Time 1` and t2.`End Time1`)
 		or (t1.`End Time1` between t2.`Begin Time 1` and t2.`End Time1`)
 	)
-	union
-	select se.`Begin Time 1`, se.`End Time1`,
-	se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`, se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`
-	from section as se, enrollment as en, student as st
-	where (se.`CRN` = en.`CRN` and se.`Term Code` = en.`Term Code`)
-	and st.`Banner ID` = en.`Banner ID`
-	and se.`Term Code` = 201610
-	and st.`Banner ID` in
-	(
-		select st.`Banner ID`
-		from section as se, student as st, enrollment as en
-		where (se.`CRN` = en.`CRN` and se.`Term Code` = en.`Term Code`)
-		and st.`Banner ID` = en.`Banner ID`
-		and se.`Subject Code` = 'CS' and se.`Course Number` = 374 and se.`Section Number` = 01
-		and se.`Term Code` = 201610
-	)
-	group by se.`Begin Time 1`, se.`End Time1`,
-	se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`, se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`
+	group by t1.`Begin Time 1`, t1.`End Time1`,
+	t1.`Monday Ind1`, t1.`Tuesday Ind1`, t1.`Wednesday Ind1`, t1.`Thursday Ind1`, t1.`Friday Ind1`, t1.`Saturday Ind1`, t1.`Sunday Ind1`
 	/* Acceptable losses: replace the zero with whatever number of students you are willing to lose */
-	having sum(st.`Class Code` = 'SR') > 0 or sum(st.`Class Code` != 'SR') > 0
+	having sum(t2.`Class Code` = 'SR') > 0 or sum(t2.`Class Code` != 'SR') > 0
 )
 and (se.`Begin Time 1`, se.`End Time1`, se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`, se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`) not in 
 (	/* Check times for instructor conflicts */
@@ -270,57 +249,3 @@ and (se.`Begin Time 1`, se.`End Time1`, se.`Monday Ind1`, se.`Tuesday Ind1`, se.
 	)
 )
 order by se.`Begin Time 1`;
-
-
-
-/* OLD */
-/* Find possible times that the class can be moved to by checking schedules of all students in a class and comparing with a list of ideal time slots */
-/* Only accounts for MWF and TR */
-/*
-select distinct se.`Begin Time 1`, se.`End Time1`,
-se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`,
-se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`
-from section as se
-where
-(
-	(se.`Monday Ind1` = 'M' and se.`Wednesday Ind1` = 'W' and se.`Friday Ind1` = 'F' and se.`Tuesday Ind1` != 'T' and se.`Thursday Ind1` != 'R' and (se.`End Time1` - se.`Begin Time 1`) = 50)
-	or (se.`Tuesday Ind1` = 'T' and se.`Thursday Ind1` = 'R' and se.`Monday Ind1` != 'M' and se.`Wednesday Ind1` != 'W' and se.`Friday Ind1` != 'F' and (se.`End Time1` - se.`Begin Time 1`) = 120)
-)
-and se.`Begin Time 1` >= 800 and se.`Begin Time 1` <= 1600
-and (se.`Begin Time 1`, se.`End Time1`, se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`, se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`) not in 
-(
-	select se.`Begin Time 1`, se.`End Time1`,
-	se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`, se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`
-	from section as se, enrollment as en, student as st
-	where (se.`CRN` = en.`CRN` and se.`Term Code` = en.`Term Code`)
-	and st.`Banner ID` = en.`Banner ID`
-	and se.`Term Code` = 201610
-	and st.`Banner ID` in
-	(
-		select st.`Banner ID`
-		from section as se, student as st, enrollment as en
-		where (se.`CRN` = en.`CRN` and se.`Term Code` = en.`Term Code`)
-		and st.`Banner ID` = en.`Banner ID`
-		and se.`Subject Code` = 'CS' and se.`Course Number` = 374 and se.`Section Number` = 01
-		and se.`Term Code` = 201610
-	)
-	group by se.`Begin Time 1`, se.`End Time1`,
-	se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`, se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`
-	having sum(st.`Class Code` = 'SR') > 0 or sum(st.`Class Code` != 'SR') > 0
-)
-and (se.`Begin Time 1`, se.`End Time1`, se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`, se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`) not in 
-(
-	select se.`Begin Time 1`, se.`End Time1`,
-	se.`Monday Ind1`, se.`Tuesday Ind1`, se.`Wednesday Ind1`, se.`Thursday Ind1`,
-	se.`Friday Ind1`, se.`Saturday Ind1`, se.`Sunday Ind1`
-	from section as se
-	where se.`Instructor ID` in
-	(
-		select se.`Instructor ID`
-		from section as se
-		where se.`Subject Code` = 'CS' and se.`Course Number` = 374 and se.`Section Number` = 01
-		and se.`Term Code` = 201610
-	)
-)
-order by se.`Begin Time 1`;
-*/
